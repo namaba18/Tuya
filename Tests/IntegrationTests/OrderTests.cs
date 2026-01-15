ac5d4fbc8ba6a15f5f27b586d92be4fef23390bd
Natalia Mantilla <natalia.mantilla@sourcecol.com>
2026-01-15 17:32:50 -0500

    Integration tests

---
+++ Tests/IntegrationTests/OrderTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+namespace Tests.IntegrationTests
+{
+    public class OrderTests : IClassFixture<CustomWebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        public OrderTests(CustomWebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+        }
+
+        [Fact]
+        public async Task GetOrders_ReturnsSuccessAndCorrectContentType()
+        {
+            var response = await _client.GetAsync("/Order");
+
+            Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
+        }
+    }
+}
